a0585269f05189fb9ae4f5abe98cd36731ad8a53
Ian Wesley-Smith <iwsmith@gmail.com>
2016-05-09 13:04:39 -0700

    Support for multiple subjects and filtering out non-wos ids

--- babel_util/scripts/json_to_pajek.py
+++ babel_util/scripts/json_to_pajek.py
@@ -9,12 +9,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Creates Pajek (.net) files from JSON")
     parser.add_argument('outfile')
     parser.add_argument('--temp-dir', help="Directory to store temporary files in", default=None)
-    parser.add_argument('--subject', '-s', help="For WoS, subject must include this.")
+    parser.add_argument('--subject', '-s', help="For WoS, subject must include this. Can be a comma seperated list.")
+    parser.add_argument('--wos-only', help="For WoS, exclude any citations or ids that contain a dot (.)", action="store_true")
     parser.add_argument('infile', nargs='+')
     arguments = parser.parse_args()
 
     b = Benchmark()
     pjk = PajekFactory(temp_dir=arguments.temp_dir)
+
+    subjects = None
+    if arguments.subject:
+        subjects = set(arguments.subject.split(","))
 
     for filename in arguments.infile:
         with open_file(filename) as f:
@@ -22,10 +27,16 @@ if __name__ == "__main__":
                 entry = ujson.loads(line)
                 b.increment()
 
-                if arguments.subject and arguments.subject not in entry["subject"]:
+                if arguments.wos_only and '.' in entry["id"]:
+                    continue
+
+                if subjects and not subject.intersection(entry["subject"]):
                     continue
 
                 for citation in entry["citations"]:
+                    if arguments.wos_only and '.' in citation:
+                        continue
+
                     pjk.add_edge(entry["id"], citation)
 
     b.print_freq()
