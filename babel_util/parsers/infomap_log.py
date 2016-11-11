4bfe902f311e07abcdb145ffe25ff67111dc3d24
Ian Wesley-Smith <iwsmith@gmail.com>
2016-11-10 18:01:21 -0800

    Finished infomap parser

--- babel_util/parsers/infomap_log.py
+++ babel_util/parsers/infomap_log.py
@@ -6,14 +6,24 @@ class InfomapLog(object):
     def __init__(self, stream):
         self._stream = stream
         self._lines = None
+        self._num_levels = None
 
     def _get_line(self, string):
         l = filter(lambda s: s.count(string), self._lines)
         assert len(l) == 1
         return l[0]
 
+    def _get_brackets(self, string, delim=',', dtype=None):
+        """For any line containing string and some [stuff], return [stuff]"""
+        l = self._get_line(string)
+        contents = l[l.index('[')+1:l.index(']')].split(delim)
+        assert len(contents) == self.num_levels()
+        if dtype:
+            return map(dtype, contents)
+        return contents
+
     def _get_stat(self, string):
-        """For any line containg string that ends ([sum|average]: 12345), get the sum/average."""
+        """For any line containing string that ends ([sum|average]: 12345), get the sum/average."""
         return self._get_line(string).split(": ")[2].strip()[:-1]
 
     def parse(self):
@@ -29,10 +39,16 @@ class InfomapLog(object):
                 "num_levels": self.num_levels(),
                 "num_modules": self.num_modules(),
                 "num_leaves": self.num_leaves(),
-                "average_child": self.average_child(),
+                "average_child_degree": self.average_child(),
                 "module_codelength": self.module_codelength(),
                 "leaf_node_codelength": self.leaf_node_codelength(),
                 "total_codelength": self.total_codelength(),
+                "per_level_num_modules": self.per_level_modules(),
+                "per_level_num_leaf_nodes": self.per_level_leaf_nodes(),
+                "per_level_avg_child_degree": self.per_level_avg_child_degree(),
+                "per_level_module_codelength": self.per_level_codelength_modules(),
+                "per_level_leaf_codelength": self.per_level_codelength_leaf_nodes(),
+                "per_level_total_codelength": self.per_level_codelength_total(),
                 }
 
     def filename(self):
@@ -58,7 +74,9 @@ class InfomapLog(object):
         return int(self._get_line("self-links.").split(" ")[3])
 
     def num_levels(self):
-        return int(self._get_line("Best end modular solution in").split(" ")[5])
+        if self._num_levels is None:
+            self._num_levels = int(self._get_line("Best end modular solution in").split(" ")[5])
+        return self._num_levels
 
     def num_modules(self):
         return int(self._get_stat("number of modules"))
@@ -77,4 +95,22 @@ class InfomapLog(object):
 
     def total_codelength(self):
         return float(self._get_stat("codelength total"))
+
+    def per_level_modules(self):
+        return self._get_brackets("Per level number of modules", dtype=int)
+
+    def per_level_leaf_nodes(self):
+        return self._get_brackets("Per level number of leaf nodes", dtype=int)
+
+    def per_level_avg_child_degree(self):
+        return self._get_brackets("Per level average child degree", dtype=float)
+
+    def per_level_codelength_modules(self):
+        return self._get_brackets("Per level codelength for modules", dtype=float)
+
+    def per_level_codelength_leaf_nodes(self):
+        return self._get_brackets("Per level codelength for leaf nodes", dtype=float)
+
+    def per_level_codelength_total(self):
+        return self._get_brackets("Per level codelength total", dtype=float)
 
--- babel_util/parsers/tests/test_infomap_log.py
+++ babel_util/parsers/tests/test_infomap_log.py
@@ -3,7 +3,7 @@ import unittest
 from parsers.infomap_log import InfomapLog
 
 L1 = "infomap.log"
-
+PARSED = {'per_level_num_modules': [9861, 4862, 47536, 121373, 46736, 4454, 184, 6, 0], 'num_leaves': 1146432, 'leaf_node_codelength': 4.043955986, 'links': 2360025, 'module_codelength': 5.224179254, 'self_links': 37, 'per_level_module_codelength': [0.068050433, 0.997671699, 2.285006274, 1.675351952, 0.188030483, 0.009791429, 0.000267017, 9.968e-06, 0.0], 'per_level_avg_child_degree': [9861.0, 5.45168, 12.0002, 9.1543, 5.24276, 3.678, 3.44701, 3.97283, 2.0], 'total_codelength': 9.26813524, 'filename': 'acoustics.net', 'average_child_degree': 76.9053, 'per_level_leaf_codelength': [0.0, 0.034513916, 0.013425651, 1.742221128, 1.928613294, 0.302656274, 0.021481528, 0.001026542, 1.7652e-05], 'per_level_total_codelength': [0.068050433, 1.032185615, 2.298431925, 3.41757308, 2.116643777, 0.312447703, 0.021748545, 0.00103651, 1.7652e-05], 'dangling_nodes': 978796, 'per_level_num_leaf_nodes': [0, 48897, 10809, 313786, 589593, 167441, 15169, 725, 12], 'nodes': 1146432, 'runtime': 141.0, 'num_levels': 9, 'num_modules': 235012}
 
 class TestInfomapLog(unittest.TestCase):
     def setUp(self):
@@ -15,46 +15,70 @@ class TestInfomapLog(unittest.TestCase):
         self._stream.close()
 
     def test_filename(self):
-        self.assertEqual("acoustics.net", self._parser.filename())
+        self.assertEqual(PARSED['filename'], self._parser.filename())
 
     def test_runtime(self):
-        self.assertEqual(141.0, self._parser.runtime())
+        self.assertEqual(PARSED['runtime'], self._parser.runtime())
 
     def test_dangling(self):
-        self.assertEqual(978796, self._parser.dangling_nodes())
+        self.assertEqual(PARSED['dangling_nodes'], self._parser.dangling_nodes())
 
     def test_nodes(self):
-        self.assertEqual(1146432, self._parser.nodes())
+        self.assertEqual(PARSED['nodes'], self._parser.nodes())
 
     def test_links(self):
-        self.assertEqual(2360025, self._parser.links())
+        self.assertEqual(PARSED['links'], self._parser.links())
 
     def test_self_links(self):
-        self.assertEqual(37, self._parser.self_links())
+        self.assertEqual(PARSED['self_links'], self._parser.self_links())
 
     def test_num_levels(self):
-        self.assertEqual(9, self._parser.num_levels())
+        self.assertEqual(PARSED['num_levels'], self._parser.num_levels())
 
     def test_num_modules(self):
-        self.assertEqual(235012, self._parser.num_modules())
+        self.assertEqual(PARSED['num_modules'], self._parser.num_modules())
 
     def test_num_leaves(self):
-        self.assertEqual(1146432, self._parser.num_leaves())
+        self.assertEqual(PARSED['num_leaves'], self._parser.num_leaves())
 
     def test_avg_child(self):
-        self.assertEqual(76.9053, self._parser.average_child())
+        self.assertEqual(PARSED['average_child_degree'], self._parser.average_child())
 
     def test_module_codelength(self):
-        self.assertEqual(5.224179254, self._parser.module_codelength())
+        self.assertEqual(PARSED['module_codelength'], self._parser.module_codelength())
 
     def test_leaf_node_codelength(self):
-        self.assertEqual(4.043955986, self._parser.leaf_node_codelength())
+        self.assertEqual(PARSED['leaf_node_codelength'], self._parser.leaf_node_codelength())
 
     def test_total_codelength(self):
-        self.assertEqual(9.268135240, self._parser.total_codelength())
+        self.assertEqual(PARSED['total_codelength'], self._parser.total_codelength())
+
+    def test_per_level_modules(self):
+        self.assertListEqual(PARSED['per_level_num_modules'],
+                             self._parser.per_level_modules())
+
+    def test_per_level_leaf_nodes(self):
+        self.assertListEqual(PARSED['per_level_num_leaf_nodes'],
+                             self._parser.per_level_leaf_nodes())
+
+    def test_per_level_avg_child_degree(self):
+        self.assertListEqual(PARSED['per_level_avg_child_degree'],
+                             self._parser.per_level_avg_child_degree())
+
+    def test_per_level_module_codelength(self):
+        self.assertListEqual(PARSED['per_level_module_codelength'],
+                             self._parser.per_level_codelength_modules())
+
+    def test_per_level_leaf_codelength(self):
+        self.assertListEqual(PARSED['per_level_leaf_codelength'],
+                             self._parser.per_level_codelength_leaf_nodes())
+
+    def test_per_level_total_codelength(self):
+        self.assertListEqual(PARSED['per_level_total_codelength'],
+                             self._parser.per_level_codelength_total())
 
     def test_parse(self):
-        print self._parser.parse()
+        self.assertDictContainsSubset(PARSED, self._parser.parse())
 
 if __name__ == '__main__':
     unittest.main()
