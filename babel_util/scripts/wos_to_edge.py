5150eecd7236615c4eab9c36fd253c7618113355
Ian Wesley-Smith <iwsmith@gmail.com>
2016-11-15 13:25:38 -0500

    Create edgelist from WoS

---
+++ babel_util/scripts/wos_to_edge.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from parsers.wos import WOSStream
+from util.PajekFactory import PajekFactory
+from util.misc import open_file, Benchmark
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="Creates Pajek (.net) files from WOS XML")
+    parser.add_argument('infile')
+    parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
+    arguments = parser.parse_args()
+
+    with open_file(parser.infile) as f:
+        p = WOSStream(f)
+        
+        for entry in p.parse():
+            for citation in entry["citations"]:
+                arguments.write("%s\t%s\n" % (entry["id"], citation))
